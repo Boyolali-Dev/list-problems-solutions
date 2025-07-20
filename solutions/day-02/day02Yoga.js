b73d15d6a296fcc2b5138312dd06c55b1bd196fa
Yoga Bayu AP <yogabayusbi@gmail.com>
2025-07-20 10:52:17 +0700

    update: remake solution day-02 by Yoga

--- solutions/day-02/day02Yoga.js
+++ solutions/day-02/day02Yoga.js
@@ -1,4 +1,16 @@
 var isAnagram = function(s, t) {
-      if (s.length !== t.length) return false;
-      return s.split('').sort().join('') === t.split('').sort().join('');
+    if (s.length !== t.length) return false;
+
+    const count = {};
+
+    for (const char of s) {
+        count[char] = (count[char] || 0) + 1;
+    }
+
+    for (const char of t) {
+        if (!count[char]) return false; 
+        count[char]--;
+    }
+
+    return true;
 };
